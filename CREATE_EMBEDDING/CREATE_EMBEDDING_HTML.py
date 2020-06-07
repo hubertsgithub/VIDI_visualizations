86972e8d4021a29481a52863a59f1fbc49f6630e
Hubert Lin <1hubertlin@gmail.com>
2020-06-07 00:40:23 -0400

    vis

--- CREATE_EMBEDDING/CREATE_EMBEDDING_HTML.py
+++ CREATE_EMBEDDING/CREATE_EMBEDDING_HTML.py
@@ -21,7 +21,7 @@ def human_format(num):
 overwrite = True
 
 ################################################################################
-input_dir = "/home/hubert/projects/VIDI/detection/model_0009999.pth_vis_conf0.5"
+input_dir = "/home/hubert/projects/VIDI/detection/model_0024999.pth_vis_conf0.5"
 ###############################################################################
 
 openseadragon_dir = "/home/hubert/projects/VIDI/VIDI_visualizations/files/openseadragon"
